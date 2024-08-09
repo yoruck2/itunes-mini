bb8e8d3c347b21b6e96b5f695efc57254e53c2e7
yoruck2 <yoruck2@gmail.com>
2024-08-09 22:40:19 +0900

    💄 SearchView 레이아웃 구성

--- itunes-mini/Features/Search/SearchView.swift
+++ itunes-mini/Features/Search/SearchView.swift
@@ -6,7 +6,32 @@
 //
 
 import UIKit
+import SnapKit
 
 class SearchView: BaseView {
     
+    lazy var searchCollectionView: UICollectionView = {
+            let layout = self.searchCollectionViewLayout()
+            let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            return collectionView
+        }()
+    let searchBar = UISearchBar()
+    
+    func searchCollectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let width = UIScreen.main.bounds.width - 10
+        layout.itemSize = CGSize(width: width, height: width * 0.22)
+        layout.scrollDirection = .vertical
+        return layout
+    }
+    
+    override func configureHierarchy() {
+        addSubview(searchBar)
+        addSubview(searchCollectionView)
+    }
+    override func configureLayout() {
+        searchCollectionView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaLayoutGuide)
+        }
+    }
 }
--- itunes-mini/Features/Search/SearchViewController.swift
+++ itunes-mini/Features/Search/SearchViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class SearchViewController: BaseViewController<SearchView> {
+    
+    let viewModel = SearchViewModel()
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-         
+        
+        NetworkManager.shared.requestSearch(api: APIURL.search(term: "카카오"))
+            .subscribe { value in
+                print("next: \(value.resultCount)")
+            } onError: { error in
+                print("error: \(error)")
+            } onCompleted: {
+                print("completed")
+            } onDisposed: {
+                print("disposed")
+            }.disposed(by: disposeBag)
     }
-
-
+    
+    override func configureViewController() {
+        rootView.searchCollectionView.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: "SearchCollectionViewCell")
+        rootView.searchCollectionView.delegate = self
+        rootView.searchCollectionView.dataSource = self
+        navigationItem.titleView = rootView.searchBar
+    }
 }
 
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCollectionViewCell", for: indexPath) as? SearchCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.appTitleLabel.text = "sdf"
+        cell.companyLabel.text = "sdfdfd"
+        cell.ratingLabel.text = "sdf"
+        cell.categoryLabel.text = "여행"
+        return cell
+    }
+}
