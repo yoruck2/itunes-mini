566de154fa747afb25058a048c8e87eea8f4a49f
yoruck2 <yoruck2@gmail.com>
2024-08-10 21:59:19 +0900

    💄searchCollectionViewLayout 조정과 searchGuideLabel추가

--- itunes-mini/Features/Search/SearchView.swift
+++ itunes-mini/Features/Search/SearchView.swift
@@ -10,26 +10,36 @@ import SnapKit
 
 class SearchView: BaseView {
     
-    lazy var searchCollectionView: UICollectionView = {
-            let layout = self.searchCollectionViewLayout()
-            let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-            return collectionView
-        }()
+    let searchGuideLabel = UILabel().then {
+        $0.text = "찾으시는 앱을 검색해주세요."
+        $0.font = .boldSystemFont(ofSize: 20)
+    }
+    
+    lazy var searchCollectionView = UICollectionView(frame: .zero, collectionViewLayout: searchCollectionViewLayout())
+    
     let searchBar = UISearchBar()
     
     func searchCollectionViewLayout() -> UICollectionViewLayout {
+        let spacing: CGFloat = 15
         let layout = UICollectionViewFlowLayout()
-        let width = UIScreen.main.bounds.width - 10
+        let width = UIScreen.main.bounds.width - spacing
+
+        layout.minimumLineSpacing = spacing
         layout.itemSize = CGSize(width: width, height: width * 0.22)
         layout.scrollDirection = .vertical
+
         return layout
     }
     
     override func configureHierarchy() {
         addSubview(searchBar)
         addSubview(searchCollectionView)
+        addSubview(searchGuideLabel)
     }
     override func configureLayout() {
+        searchGuideLabel.snp.makeConstraints { make in
+            make.center.equalTo(searchCollectionView)
+        }
         searchCollectionView.snp.makeConstraints { make in
             make.edges.equalTo(safeAreaLayoutGuide)
         }
