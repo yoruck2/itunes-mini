46330ab98fb96798979654b99f14326c5c60bcbb
yoruck2 <yoruck2@gmail.com>
2024-08-11 23:58:54 +0900

    ✨ DetailCollectionViewCell 구현

--- itunes-mini/Features/Search/Detail/DetailCollectionViewCell.swift
+++ itunes-mini/Features/Search/Detail/DetailCollectionViewCell.swift
@@ -5,4 +5,30 @@
 //  Created by dopamint on 8/10/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Kingfisher
+
+class DetailCollectionViewCell: BaseCollectionViewCell {
+    
+    let screenShotImageView = UIImageView()
+    
+    
+    func setUpCellImage(data:URL) {
+        screenShotImageView.kf.setImage(with: data)
+    }
+    
+    override func configureHierarchy() {
+        addSubview(screenShotImageView)
+    }
+    
+    override func configureLayout() {
+        screenShotImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    override func configureView() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+    }
+}
